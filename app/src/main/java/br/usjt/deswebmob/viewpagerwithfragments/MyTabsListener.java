20dfe9350d22c98e92d802f7063abaf8512c008f
arqdsis <arqdsis@mocli10228.animaedu.intranet>
2018-04-19 20:54:58 -0300

    commit inicial

---
+++ app/src/main/java/br/usjt/deswebmob/viewpagerwithfragments/MyTabsListener.java
@@ -0,0 +1,36 @@
+package br.usjt.deswebmob.viewpagerwithfragments;
+
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBar.TabListener;
+
+/**
+ * Created by arqdsis on 19/04/2018.
+ */
+
+public class MyTabsListener implements TabListener {
+
+    private ViewPager viewPager;
+    private int index;
+
+    public MyTabsListener(ViewPager viewPager, int index) {
+        this.viewPager = viewPager;
+        this.index = index;
+    }
+
+    @Override
+    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
+        viewPager.setCurrentItem(index);
+    }
+
+    @Override
+    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
+
+    }
+
+    @Override
+    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
+
+    }
+}
